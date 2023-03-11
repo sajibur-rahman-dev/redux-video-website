a06f77a1384e4a453fb5183a947454cb5469497f
sajibur-rahman-dev <sajibur.dev@gmail.com>
2023-03-11 09:36:48 +0600

    fetch tags is done

--- src/components/Tags/Tag.js
+++ src/components/Tags/Tag.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
-const Tag = ({tag}) => {
-    const {title} = tag;
+const Tag = ({title}) => {
     return (
         <div
             className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer"
--- src/components/Tags/Tags.js
+++ src/components/Tags/Tags.js
@@ -1,36 +1,30 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchTags } from '../../features/tags/tagsSlice';
-import Loading from '../ui/Loading';
 import Tag from './Tag'
 
 const Tags = () => {
     const dispatch = useDispatch();
-    const {isLoading,tags,isError,error}= useSelector(state => state.tags);
-    useEffect(()=>{
+    const { tags } = useSelector(state => state.tags);
+    useEffect(() => {
         dispatch(fetchTags());
-    },[dispatch]);
+    }, [dispatch]);
 
 
-    let content;
-
-    if(isLoading) content = <Loading/>;
-
-    if(!isLoading && isError) content = <div className="col-span-12">{error}</div>;
-
-    if(!isLoading && !isError && tags?.length === 0) content = <div className="col-span-12">No data found</div>
-
-    if(!isLoading && !isError && tags?.length > 0) content = tags.map((tag) =>  <Tag key={tag.id} tag={tag} />)
-
-    return (
+    return tags?.length > 0 ? (
         <section>
             <div
                 className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex gap-2 border-b overflow-y-auto"
             >
-                {content}
+
+                {
+                    tags.map((tag) =>
+                        <Tag key={tag.id} title={tag.title}
+                        />)
+                }
             </div>
         </section>
-    )
+    ) : null;
 }
 
 export default Tags
